e62f7fa3d158c4ba99105d9773919c040eac64e9
WalterNadalin <walternadalin4@gmail.com>
2024-02-29 13:59:43 +0100

    Cleaning

--- sorting/algorithms/heap_sort.py
+++
@@ -1,7 +0,0 @@
-def heap_sort(array, begin, end):
-    heap_array = data_structures.heap(*array, max_heap = True)
-
-    for i in range(end, begin + 1, - 1):
-        array[i] = heap_array.pop_minimum()
-
-    array[0], array[1] = heap_array[1], heap_array[0]
